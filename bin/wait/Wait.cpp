3be54c0c99a3872ecf31eb071ed1a75d5eabda2f
g4tr <natc160@gmail.com>
2023-10-02 02:19:51 -0400

    Finish project

--- bin/wait/Wait.cpp
+++ bin/wait/Wait.cpp
@@ -1,32 +1,17 @@
-/*
- * Copyright (C) 2009 Niek Linnenbank
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h> // Include for waitpid
 #include "Wait.h"
 
 Wait::Wait(int argc, char **argv)
     : POSIXApplication(argc, argv)
 {
-    parser().setDescription("Stop executing for some time");
-    parser().registerPositional("SECONDS", "Stop executing for the given number of seconds");
+    parser().setDescription("Wait for process");
+    parser().registerPositional("PID", "Process ID");
 }
 
 Wait::~Wait()
@@ -35,22 +20,19 @@ Wait::~Wait()
 
 Wait::Result Wait::exec()
 {
-    int sec = 0;
+    int status;
+    pid_t pid;
+    
+    // get pid from command line
+    pid = atoi(arguments().get("PID"));
 
-    // Convert input to seconds
-    if ((sec = atoi(arguments().get("SECONDS"))) <= 0)
+    if (waitpid(pid, &status, 0) == (pid_t)-1)
     {
-        ERROR("invalid wait time `" << arguments().get("SECONDS") << "'");
-        return InvalidArgument;
-    }
-
-    // Wait now
-    if (sleep(sec) != 0)
-    {
-        ERROR("failed to wait: " << strerror(errno));
+        ERROR("Failed to wait: " << strerror(errno));
         return IOError;
     }
 
     // Done
     return Success;
 }
+
--- bin/wait/Wait.h
+++ bin/wait/Wait.h
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2009 Niek Linnenbank
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #ifndef __BIN_WAIT_WAIT_H
 #define __BIN_WAIT_WAIT_H
 
@@ -26,12 +9,11 @@
  */
 
 /**
- * Stop executing for some time.
+ * Wait for a specific process to finish.
  */
 class Wait : public POSIXApplication
 {
-  public:
-
+public:
     /**
      * Constructor
      *
@@ -58,3 +40,4 @@ class Wait : public POSIXApplication
  */
 
 #endif /* __BIN_WAIT_WAIT_H */
+
